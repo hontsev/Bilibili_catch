ca0726f5e26861bc4912b460f4a33c9c57ac4634
hontsev <hontsev@gmail.com>
2016-09-10 10:23:27 +0800

    files

---
+++ Bilibili_catch/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace Bilibili_catch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.wb = new System.Windows.Forms.WebBrowser();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 11);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "扫描弹幕";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 40);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(70, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 75);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(223, 180);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(88, 13);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(73, 21);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1";
+            // 
+            // wb
+            // 
+            this.wb.Location = new System.Drawing.Point(251, 11);
+            this.wb.MinimumSize = new System.Drawing.Size(20, 20);
+            this.wb.Name = "wb";
+            this.wb.Size = new System.Drawing.Size(257, 54);
+            this.wb.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(88, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(73, 21);
+            this.textBox3.TabIndex = 5;
+            this.textBox3.Text = "1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(167, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 12);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "←起始id";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(168, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "←弹幕池个数";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(350, 81);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(41, 36);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "→";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(273, 95);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(62, 21);
+            this.textBox4.TabIndex = 9;
+            this.textBox4.Text = "1";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(432, 95);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(62, 21);
+            this.textBox5.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 261);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "状态信息：";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(74, 261);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "无";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(241, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "视频id：";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(402, 80);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(53, 12);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "弹幕id：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 282);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.wb);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "获取Bilibili的弹幕信息 _(:з」∠)_";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.WebBrowser wb;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Bilibili_catch/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Net;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace Bilibili_catch
+{
+    public partial class Form1 : Form
+    {
+        private string xmlid;
+        private FileStream output;
+        private StreamWriter w;
+        private Boolean isrun=false;
+        private int sum1 = 0;
+        private int sum2 = 0;
+        public Form1()
+        {
+            xmlid = "";
+            InitializeComponent();
+            wb.ScriptErrorsSuppressed = true;
+        }
+        private void printline(string str)
+        {
+            textBox1.Text = textBox1.Text + str + "\r\n";
+            textBox1.Select(textBox1.TextLength, 0);
+            textBox1.ScrollToCaret();
+        }
+        private void print(string str)
+        {
+            textBox1.Text = textBox1.Text + str;
+            textBox1.Select(textBox1.TextLength, 0);
+            textBox1.ScrollToCaret();
+        }
+        private void printstate(string str)
+        {
+            label4.Text = str;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //开始扫
+            xmlid = textBox2.Text;
+            printline(xmlid);
+            printline("_____开始_____");
+            printstate("扫描中..");
+            sum1 = sum2 = 0;
+            output = new FileStream("output.txt", FileMode.Create, FileAccess.Write);
+            w = new StreamWriter(output);
+            isrun = true;
+            int tid = Int32.Parse(textBox2.Text);
+            int times = Int32.Parse(textBox3.Text);
+            while(isrun==true && times-- > 0)
+            {
+                getxmlinfo("http://comment.bilibili.tv/" + tid + ".xml");
+                tid++;
+                sum1++;
+            }
+            printline("_____结束_____");
+            printline("共计" + sum1 + "个视频，" + sum2 + "条弹幕。已经存入exe文件同目录下的output.txt中。请查收_(:з」∠)_");
+            printstate("扫描完毕。共计" + sum1 + "个视频，" + sum2 + "条弹幕。");
+            textBox2.Text = tid.ToString();
+            w.Close();
+            output.Close();
+        }
+        private void getxmlinfo(string url)
+        {
+            string res = "";
+            wb.Navigate(url);
+            while (wb.ReadyState != WebBrowserReadyState.Complete) Application.DoEvents();
+            res = wb.DocumentText;
+            HtmlDocument doc = wb.Document;
+            //Regex reg = new Regex(@"</B><SPAN class=""m"">""</SPAN><SPAN class=""m"">&gt;</SPAN><SPAN class=""tx"">([^<]*)</SPAN>");
+            MatchCollection mc = Regex.Matches(res, @"</B><SPAN class=""m"">""</SPAN><SPAN class=""m"">&gt;</SPAN><SPAN class=""tx"">([^<]*)</SPAN>", RegexOptions.IgnoreCase);  
+            string[] content = new string[mc.Count];
+            for(int i=0;i<content.Length;i++)
+            {
+                content[i] = mc[i].Groups[1].Value;
+                w.WriteLine(content[i]);
+                sum2++;
+                Application.DoEvents();
+            }
+            printline("finish:"+(sum1+1).ToString());
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //停止扫
+            isrun = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //按视频id获取弹幕id
+            string vid = textBox4.Text;
+            string url = "http://www.bilibili.com/video/av"+vid;
+            //string url = "http://www.baidu.com";
+            string res = "";
+            wb.Navigate(url);
+            while (wb.ReadyState != WebBrowserReadyState.Complete)
+            {
+                Application.DoEvents();
+            }
+            res = wb.DocumentText;
+            HtmlDocument doc = wb.Document;
+            MatchCollection mc = Regex.Matches(res, @"cid=([^<]*)&aid=", RegexOptions.IgnoreCase);
+            if(mc.Count>0)
+            {
+                res = mc[0].Groups[1].Value;
+                textBox5.Text = res;
+                printstate("获取弹幕id完毕");
+            }
+            else
+            {
+                textBox5.Text = "";
+                printstate("获取弹幕id失败");
+            }
+        }
+    }
+}
